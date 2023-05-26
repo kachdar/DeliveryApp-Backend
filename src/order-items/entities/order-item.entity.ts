2520bfc52958301d37a106893c53a461daea044b
Daria <kachynska.daria@lll.kpi.ua>
2023-05-26 23:20:11 +0300

    fix

--- src/order-items/entities/order-item.entity.ts
+++ src/order-items/entities/order-item.entity.ts
@@ -16,19 +16,19 @@ export class OrderItem {
   @Column({ type: 'numeric' })
   quantity: number;
 
-  @Column()
-  productId: number;
+  // @Column()
+  // productId: number;
   @ManyToOne(() => Product, (product) => product.orderItems)
-  @JoinColumn({ name: 'productId' })
+  // @JoinColumn({ name: 'productId' })
   product: Product;
 
-  @Column()
-  orderId: number;
+  // @Column()
+  // orderId: number;
   @ManyToOne(() => Order, (order) => order.orderItems, {
     eager: false,
     cascade: true,
     onDelete: 'CASCADE',
   })
-  @JoinColumn({ name: 'orderId' })
+  // @JoinColumn({ name: 'orderId' })
   order: Order;
 }
