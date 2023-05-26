6a4cb0c67bbcbb2231b3f95e188034c3a2f0bba4
Daria <kachynska.daria@lll.kpi.ua>
2023-05-26 23:01:09 +0300

    update

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -4,16 +4,25 @@ import { UpdateOrderDto } from './dto/update-order.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
+import { OrderItem } from 'src/order-items/entities/order-item.entity';
 
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectRepository(Order)
     private orderRepository: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private orderItemsRepository: Repository<OrderItem>,
   ) {}
 
-  create(createOrderDto: CreateOrderDto) {
-    return this.orderRepository.save(createOrderDto);
+  async create(createOrderDto: CreateOrderDto) {
+    const order = await this.orderRepository.save(createOrderDto);
+
+    createOrderDto.orderItems.forEach((item) =>
+      this.orderItemsRepository.save({ ...item, orderId: order.id }),
+    );
+
+    return order;
   }
 
   findAll() {
