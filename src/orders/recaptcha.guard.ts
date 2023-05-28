0c21de45cf942efb9f4b0eb5afdc35886bba4378
Daria <kachynska.daria@lll.kpi.ua>
2023-05-28 17:12:50 +0300

    up

--- src/orders/recaptcha.guard.ts
+++ src/orders/recaptcha.guard.ts
@@ -12,16 +12,16 @@ export class RecaptchaGuard implements CanActivate {
   constructor(private readonly httpService: HttpService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const { body } = context.switchToHttp().getRequest();
-    console.log(body);
+    const { recaptcha } = context.switchToHttp().getRequest();
+    console.log(recaptcha);
     const secretKey = '6Leo80YmAAAAAGE6BkzWhFTLdgvH9rDyLwMJ40tc';
-    const url = `https://www.google.com/recaptcha/api/siteverify?response=${body.recaptcha}&secret=${secretKey}`;
+    const url = `https://www.google.com/recaptcha/api/siteverify?response=${recaptcha}&secret=${secretKey}`;
     const { data } = await lastValueFrom(this.httpService.post(url));
 
     console.log(data.success);
 
     if (!data.success) {
-      throw new ForbiddenException(`${body.recaptcha}, ${secretKey}, ${data}`);
+      throw new ForbiddenException(`${recaptcha}, ${secretKey}, ${data}`);
     }
 
     return true;
