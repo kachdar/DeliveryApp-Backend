73904d9a3593cf4412a137ce77d08c6a40d3fcd1
Daria <kachynska.daria@lll.kpi.ua>
2023-05-28 05:42:51 +0300

    fix import

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,9 +4,10 @@ import { OrdersController } from './orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { OrderItem } from 'src/order-items/entities/order-item.entity';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem]), HttpModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
