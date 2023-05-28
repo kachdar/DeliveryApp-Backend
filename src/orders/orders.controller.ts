68bcf35665ea9d11fce2e1571bf4fc77ff5bdeda
Daria <kachynska.daria@lll.kpi.ua>
2023-05-28 05:28:45 +0300

    add recaptcha

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { RecaptchaGuard } from './recaptcha.guard';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
+  @UseGuards(RecaptchaGuard)
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
---
+++ src/orders/recaptcha.guard.ts
@@ -0,0 +1,26 @@
+import { HttpService } from '@nestjs/axios';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class RecaptchaGuard implements CanActivate {
+  constructor(private readonly httpService: HttpService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const { body } = context.switchToHttp().getRequest();
+    const secretKey = '6Leo80YmAAAAAGE6BkzWhFTLdgvH9rDyLwMJ40tc';
+    const url = `https://www.google.com/recaptcha/api/siteverify?response=${body.token}&secret=${secretKey}`;
+    const { data } = await firstValueFrom(this.httpService.post(url));
+
+    if (!data.success || data.score < 0.99) {
+      throw new ForbiddenException();
+    }
+
+    return true;
+  }
+}
