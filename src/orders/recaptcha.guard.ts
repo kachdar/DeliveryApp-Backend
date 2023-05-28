437d5c5c305df38a0e6e8e7ce98a4905d942b441
Daria <kachynska.daria@lll.kpi.ua>
2023-05-28 17:15:23 +0300

    try

--- src/orders/recaptcha.guard.ts
+++ src/orders/recaptcha.guard.ts
@@ -12,18 +12,18 @@ export class RecaptchaGuard implements CanActivate {
   constructor(private readonly httpService: HttpService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const { recaptcha } = context.switchToHttp().getRequest();
-    console.log(recaptcha);
+    const res = context.switchToHttp().getRequest();
+    console.log(res);
     const secretKey = '6Leo80YmAAAAAGE6BkzWhFTLdgvH9rDyLwMJ40tc';
-    const url = `https://www.google.com/recaptcha/api/siteverify?response=${recaptcha}&secret=${secretKey}`;
-    const { data } = await lastValueFrom(this.httpService.post(url));
+    //const url = `https://www.google.com/recaptcha/api/siteverify?response=${recaptcha}&secret=${secretKey}`;
+    //const { data } = await lastValueFrom(this.httpService.post(url));
 
-    console.log(data.success);
+    // console.log(data.success);
 
-    if (!data.success) {
-      throw new ForbiddenException(`${recaptcha}, ${secretKey}, ${data}`);
-    }
+    // if (!data.success) {
+    //   throw new ForbiddenException(`${recaptcha}, ${secretKey}, ${data}`);
+    // }
 
-    return true;
+    return res;
   }
 }
