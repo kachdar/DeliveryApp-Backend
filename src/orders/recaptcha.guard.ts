3aecc5aefc1e421e70f4b80165b60890c10a041d
Daria <kachynska.daria@lll.kpi.ua>
2023-05-28 18:04:05 +0300

    fix recaptcha

--- src/orders/recaptcha.guard.ts
+++ src/orders/recaptcha.guard.ts
@@ -5,7 +5,7 @@ import {
   ExecutionContext,
   ForbiddenException,
 } from '@nestjs/common';
-import { catchError, firstValueFrom, lastValueFrom, map } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class RecaptchaGuard implements CanActivate {
@@ -16,15 +16,13 @@ export class RecaptchaGuard implements CanActivate {
 
     const secretKey = '6Leo80YmAAAAAGE6BkzWhFTLdgvH9rDyLwMJ40tc';
     const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${res.body.recaptcha}`;
-    console.log(url);
 
     const response = await lastValueFrom(this.httpService.post(url));
-    console.log(response.data)
 
-    // if (!data.success) {
-    //   throw new ForbiddenException(`${recaptcha}, ${secretKey}, ${data}`);
-    // }
+    if (!response.data.success) {
+      throw new ForbiddenException();
+    }
 
-    return res;
+    return true;
   }
 }
